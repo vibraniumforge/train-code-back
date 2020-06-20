5e236849945f5cb81d0c64db311feffc50e857dd
vibraniumforge <gregm9999@gmail.com>
2020-06-19 19:25:54 -0700

    commit 3

--- app/controllers/api/v1/trains_controller.rb
+++ app/controllers/api/v1/trains_controller.rb
@@ -4,13 +4,14 @@ module Api::V1
     def show
       @train = Train.find(params[:id])
       render json: @train
+      # render json: { data: @train, success: true, message: "OK" }
     end
 
     def get_train_by_symbol
       @train = Train.find_by_code(params[:symbol])
-      puts "train = #{@train.pluck(:id)}"
+      # puts "train = #{@train.pluck(:id)}"
       if @train.length > 0
-        render json: {data: @train, success: true, message: "OK"}
+        render json: { data: @train, success: true, message: "OK" }
       else
         render json: { success: false, message: "No trains found"}
       end
--- app/models/train.rb
+++ app/models/train.rb
@@ -2,7 +2,7 @@ class Train < ApplicationRecord
 
   def self.find_by_code(symbol)
     # Train.where(symbol: symbol)
-    Train.where("symbol LIKE ?", "%#{symbol}%")
+    Train.where("symbol LIKE ?", "#{symbol}%")
   end
 
 end
